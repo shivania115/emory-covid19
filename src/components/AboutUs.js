239fadc1907c3afd025e39205ad158b12aebdd4c
StarLiu1 <star.liu@emory.edu>
2020-07-27 16:57:26 -0400

    Update AboutUs.js

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -3,106 +3,100 @@ import AppBar from './AppBar';
 import Notes from './Notes';
 import { Container, Grid, List, Divider, Image, Header, Segment } from 'semantic-ui-react'
 import {CopyToClipboard} from 'react-copy-to-clipboard';
-
-
 function myFunction() {
   alert('Link is copied to clipboard!');
   
 }
+
 export default function AboutUs(props){
 
   return (
     <div>
       <AppBar menu='aboutUs'/>
       <Container style={{marginTop: '8em', minWidth: '1260px'}}>
-        <Header as='h1' style={{fontWeight: 400}}>
-          <Header.Content>
-            About This Dashboard
-            <Header.Subheader style={{color: "#000000", paddingTop: 15}}>Early data about COVID-19 suggests that communities are affected very differently due to social determinants of health like population density, poverty, residential segregation, underlying chronic health conditions, and availability of medical services. 
-          In order to predict how the epidemic will continue to unfold and prepare for the future, it is critical to understand differences in underlying risk factors. 
-          There is no one-size-fits all approach to combat the epidemic, but accurate and meaningful data is a key component of a robust public health response that is informed by contextual factors and prioritizes health equity.
-          <br/><br/>
-          The COVID-19 Health Equity Dashboard (<a href="COVID19.emory.edu"> COVID19.emory.edu</a>) seeks to fill the gaps in county-level data about the virus and underlying social determinants of health. 
-          Our goal is for this Dashboard to facilitate easy comparisons of counties with respect to COVID-19 outcomes and social determinants. 
-          We hope this becomes a valuable resource for and critical component of tailored public health responses to COVID-19 across the wide range of environments that Americans inhabit.</Header.Subheader>
-          </Header.Content>
-        </Header>
-        <Divider hidden/>
-        <Divider hidden/>
-        <Grid.Row>
-        <Grid.Column style={{fontSize: '1em', fontWeight: 300, lineHeight: '1.5em'}}>
 
-        <Header as='h2' style={{fontWeight: 400}}>
-          <Header.Content>
-            Our Team
-            <Header.Subheader style = {{color: "#000000", paddingTop: 15, paddingBottom: 10}}>
-              We are a group of epidemiologists, doctors, and software engineers from Emory University and University of Michigan.
+        
+        <Grid columns={2}>
+          <Grid row = {2} style = {{marginLeft: -10, width: 690, paddingTop: 20}}>
+            <Header style={{fontWeight: 400, fontSize: "24pt", paddingBottom: 0}}>
+                About This Dashboard
+                <Header.Subheader style={{color: "#000000", paddingTop: 15, lineHeight: "22pt", fontSize: "12pt"}}>Early data about COVID-19 suggests that communities are affected very differently due to social determinants of health like population density, poverty, residential segregation, underlying chronic health conditions, and availability of medical services. 
+                  In order to predict how the epidemic will continue to unfold and prepare for the future, it is critical to understand differences in underlying risk factors. 
+                  There is no one-size-fits all approach to combat the epidemic, but accurate and meaningful data is a key component of a robust public health response that is informed by contextual factors and prioritizes health equity.
+                  <br/><br/>
+                  The COVID-19 Health Equity Dashboard (<a href="COVID19.emory.edu"> COVID19.emory.edu</a>) seeks to fill the gaps in county-level data about the virus and underlying social determinants of health. 
+                  Our goal is for this Dashboard to facilitate easy comparisons of counties with respect to COVID-19 outcomes and social determinants. 
+                  We hope this becomes a valuable resource for and critical component of tailored public health responses to COVID-19 across the wide range of environments that Americans inhabit.
+                </Header.Subheader>
+            </Header>
 
-            </Header.Subheader>
-          </Header.Content>
-        </Header>
+            <Header style={{fontWeight: 400, fontSize: "18pt", paddingTop: 45}}>
+                Funding
+              <Header.Content style = {{paddingTop: 10, lineHeight: "22pt", paddingBottom: 20, fontSize: "12pt"}}>
+                The COVID-19 Health Equity Dashboard was developed with funding from the Robert Wood Johnson Foundation and Emory University’s Woodruff Health Sciences Center. Development was also supported by the Georgia Center for Diabetes Translation Research.
+              </Header.Content>
+            </Header>
+          </Grid>
 
-        </Grid.Column>
-      </Grid.Row>
-        <Grid columns={3}>
-          <Grid.Column>
-            <List bulleted>
-              <List.Item>Shivani A. Patel, PhD</List.Item>
-              <List.Item>K. M. Venkat Narayan, MD</List.Item>
-              <List.Item>Carlos Del Rio, MD</List.Item>
-              <List.Item>Mark Hutcheson, BS</List.Item>
-              <List.Item>Yubin Park, PhD</List.Item>
-              <List.Item>Daesung Choi, PhD</List.Item>
-              <List.Item>Pooja Naik, B.Pharm</List.Item>
-              <List.Item>Star Liu</List.Item>
-            </List>
-          </Grid.Column>
-          <Grid.Column>
-            <List bulleted>
-              <List.Item>Leanna Ehrlich, BA</List.Item>
-              <List.Item>Neil K. Mehta, PhD</List.Item>
-              <List.Item>Vince Marconi, MD</List.Item>
-              <List.Item>Michael Kramer, PhD</List.Item>
-              <List.Item>Rob O'Reilly, PhD</List.Item>
-              <List.Item>Sanjana Pampati, MPH</List.Item>
-              <List.Item>Joyce Ho, PhD</List.Item>
-              <List.Item>Shabatun Jamila Islam, MD</List.Item>
-            </List>
-          </Grid.Column>
-          <Grid.Column>
-            <List bulleted>
-              <List.Item>Anurag Mehta, MD</List.Item>
-              <List.Item>Aditi Nayak , MD</List.Item>
-              <List.Item>Arshed Quyyumi , MD</List.Item>
-              <List.Item>Yi-Ann Co, PhD</List.Item>
-              <List.Item>Samaah Sullivan, MD</List.Item>
-              <List.Item>Mohammed K. Ali, MD</List.Item>
-            </List>
-          </Grid.Column>
+
+          <Grid columns={2} style = {{width : 600, marginLeft: 20}}>
+            <Header as='h1' style={{fontWeight: 400, width: 600}}>
+              <Header.Content style = {{ fontSize: "18pt", paddingTop: 44, lineHeight: "22pt"}}>
+                About Team
+                <Header.Subheader style={{color: "#000000", paddingTop: 10, paddingBottom: 0, lineHeight: "24pt"}}>We are a group of epidemiologists, doctors, and software engineers from Emory University and University of Michigan.</Header.Subheader>
+              </Header.Content>
+            </Header>
+            <Grid.Column>
+              <List bulleted style = {{lineHeight: "18pt", fontSize: "12pt"}}>
+                <List.Item style = {{lineHeight: "18pt", fontSize: "12pt"}}>Shivani A. Patel, PhD</List.Item>
+                <List.Item style = {{lineHeight: "18pt", fontSize: "12pt"}}>K. M. Venkat Narayan, MD</List.Item>
+                <List.Item style = {{lineHeight: "18pt", fontSize: "12pt"}}>Carlos Del Rio, MD</List.Item>
+                <List.Item style = {{lineHeight: "18pt", fontSize: "12pt"}}>Mark Hutcheson, BS</List.Item>
+                <List.Item style = {{lineHeight: "18pt", fontSize: "12pt"}}>Yubin Park, PhD</List.Item>
+                <List.Item style = {{lineHeight: "18pt", fontSize: "12pt"}}>Daesung Choi, PhD</List.Item>
+                <List.Item style = {{lineHeight: "18pt", fontSize: "12pt"}}>Pooja Naik, B.Pharm</List.Item>
+                <List.Item style = {{lineHeight: "18pt", fontSize: "12pt"}}>Star Liu</List.Item>
+                <List.Item style = {{lineHeight: "18pt", fontSize: "12pt"}}>Leanna Ehrlich, BA</List.Item>
+                <List.Item style = {{lineHeight: "18pt", fontSize: "12pt"}}>Neil K. Mehta, PhD</List.Item>
+                <List.Item style = {{lineHeight: "18pt", fontSize: "12pt"}}>Vince Marconi, MD</List.Item>
+                
+              </List>
+            </Grid.Column>
+            <Grid.Column>
+              <List bulleted style = {{lineHeight: "18pt", fontSize: "12pt"}}>
+                <List.Item style = {{lineHeight: "18pt", fontSize: "12pt"}}>Michael Kramer, PhD</List.Item>
+                <List.Item style = {{lineHeight: "18pt", fontSize: "12pt"}}>Rob O'Reilly, PhD</List.Item>
+                <List.Item style = {{lineHeight: "18pt", fontSize: "12pt"}}>Sanjana Pampati, MPH</List.Item>
+                <List.Item style = {{lineHeight: "18pt", fontSize: "12pt"}}>Joyce Ho, PhD</List.Item>
+                <List.Item style = {{lineHeight: "18pt", fontSize: "12pt"}}>Shabatun Jamila Islam, MD</List.Item>
+                <List.Item style = {{lineHeight: "18pt", fontSize: "12pt"}}>Anurag Mehta, MD</List.Item>
+                <List.Item style = {{lineHeight: "18pt", fontSize: "12pt"}}>Aditi Nayak , MD</List.Item>
+                <List.Item style = {{lineHeight: "18pt", fontSize: "12pt"}}>Arshed Quyyumi , MD</List.Item>
+                <List.Item style = {{lineHeight: "18pt", fontSize: "12pt"}}>Yi-Ann Co, PhD</List.Item>
+                <List.Item style = {{lineHeight: "18pt", fontSize: "12pt"}}>Samaah Sullivan, MD</List.Item>
+                <List.Item style = {{lineHeight: "18pt", fontSize: "12pt"}}>Mohammed K. Ali, MD</List.Item>
+
+              </List>
+            </Grid.Column>
+
+            <Header style={{fontWeight: 400, fontSize: "18pt", paddingBottom: 0}}>
+              Feedback
+              <Header.Content style = {{paddingTop: 10, lineHeight: "22pt", fontSize: "12pt"}}>
+                We strive to make this a user-friendly resource for policy makers, public health actors, the public, researchers, 
+                and the media. Please share your feedback by mailing us at: 
+                <a href="mailto:covid19dashboard@emory.edu"> covid19dashboard@emory.edu</a>
+              </Header.Content>
+            </Header>
+          </Grid> 
+
+          
+
         </Grid>
-        <Divider hidden/>
-        <Header as='h2' style={{fontWeight: 400}}>
-          <Header.Content>
-            Funding
-          </Header.Content>
-        </Header>
-        <p>The COVID-19 Health Equity Dashboard was developed with funding from the Robert Wood Johnson Foundation and Emory University’s Woodruff Health Sciences Center. Development was also supported by the Georgia Center for Diabetes Translation Research.</p>
-        <Divider hidden/>
-        <Header as='h2' style={{fontWeight: 400}}>
-          <Header.Content>
-            Feedback
-          </Header.Content>
-        </Header>
-        <p>We strive to make this a user-friendly resource for policy makers, public health actors, the public, researchers, 
-          and the media. Please share your feedback by mailing us at: 
-          <a href="mailto:covid19dashboard@emory.edu"> covid19dashboard@emory.edu</a>
-        </p>
-
-        <Divider/>
+        
 
         <div style={{paddingTop: '2em', fontWeight: 300}}>
+    <Divider/>
     <Grid>
-      
       <Divider hidden/>
       <Grid.Row columns={3}>
         <Grid.Column>          
@@ -161,7 +155,6 @@ export default function AboutUs(props){
 
 
     </div>
-        
 
       </Container>
     </div>);
