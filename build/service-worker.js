2b11d02c615fd799a6e919209e754c52328fd01c
StarLiu1 <star.liu@emory.edu>
2020-08-19 20:09:19 -0400

    deployed

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.72ede55c22645135cfe513df6f7cb159.js"
+  "/precache-manifest.86a122bf103c88d03805c08a88e9125c.js"
 );
 
 self.addEventListener('message', (event) => {
