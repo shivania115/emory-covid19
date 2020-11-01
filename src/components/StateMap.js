d8ae8566d9ed196cb130cc1442276aa2f33c9e46
StarLiu1 <star.liu@emory.edu>
2020-11-01 02:49:44 -0500

    fix

--- src/components/StateMap.js
+++ src/components/StateMap.js
@@ -719,8 +719,12 @@ export default function StateMap(props) {
                                                                                                       !!raceData[stateFips]["American Natives Alone"]
                                                                                                       && !raceData[stateFips]["Hispanic"]? "Race" : "Race & Ethnicity")}</center>
                }
-              {stateFips === "38" || stateFips === "02" &&
+              {stateFips === "38" &&
                 <div style = {{background: "#e5f2f7", paddingBottom: 61}}> <center style = {{ fontSize: "16pt", fontFamily: "lato", paddingBottom: 5}}> <br/> <br/> <br/> <br/> None Reported <br/>  <br/> </center></div>
+              }
+
+              {stateFips === "02" &&
+                <div style = {{background: "#e5f2f7", paddingBottom: 13}}> <center style = {{ fontSize: "16pt", fontFamily: "lato", paddingBottom: 5}}> <br/> <br/> <br/>None Reported <br/>  <br/> </center></div>
               }
               
               <div style = {{width: 235, background: "#e5f2f7"}}>
--- src/components/USMapPilot.js
+++ src/components/USMapPilot.js
@@ -775,7 +775,10 @@ export default function USMap(props) {
                         <div style = {{marginTop: 10}}>
                           <text x={0} y={20} style={{fontSize: '14pt', paddingLeft: 55, fontWeight: 400}}> Deaths by Race</text>
 
-                            <center> <text x={0} y={20} style={{fontSize: '14pt', paddingLeft: 0, fontWeight: 400}}> <br/> <br/> None Reported</text> </center>
+                           <text x={0} y={20} style={{fontSize: '14pt', paddingLeft: 0, fontWeight: 400}}> <br/> <br/> <br/> 
+                           &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                           &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                           &nbsp;&nbsp;None Reported</text>
                         </div>
                       }
 
