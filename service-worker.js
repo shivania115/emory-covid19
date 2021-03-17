97b95bbd490fe4c6ea12ee15899f2fb14f9d3109
StarLiu1 <star.liu@emory.edu>
2021-03-17 19:38:30 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.fc42e810ee76a6fa7e746426ab80f529.js"
+  "/precache-manifest.3a7a77941d47f0d81af239894ed5f64f.js"
 );
 
 self.addEventListener('message', (event) => {
