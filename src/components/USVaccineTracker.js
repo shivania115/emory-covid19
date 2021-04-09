3c03a065d12351ceb82f2f86c5b96798a08697fa
StarLiu1 <star.liu@emory.edu>
2021-04-09 16:19:17 -0400

    Update USVaccineTracker.js

--- src/components/USVaccineTracker.js
+++ src/components/USVaccineTracker.js
@@ -2550,7 +2550,7 @@ const USVaccineTracker = (props) => {
                                             The United States reports deaths by combined race and ethnicity groups. The chart shows race and ethnicity groups that constitute at least 1% of the state population and have 30 or more deaths. Race and ethnicity data are known for {nationalDemog['race'][0]['Unknown'][0]['availableDeaths'] + "%"} of deaths in the nation.
                                             <br/>
                                             <br/> <i>Data source</i>: <a style ={{color: "#397AB9"}} href = "https://covid.cdc.gov/covid-data-tracker/#demographics" target = "_blank" rel="noopener noreferrer"> The CDC </a>
-                                            <br/><b>Data as of:</b> 03/15/2021, updated every weekday.<br/>
+                                            <br/><b>Data as of:</b> {date}, updated every weekday.<br/>
                                           
                                           </Header.Content>
                                         </Grid.Row>}
