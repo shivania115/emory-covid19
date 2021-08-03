01af0573caacdfaa1e1235aa536189323651fe5f
Kevin Wu <kwu78@emory.edu>
2021-08-03 13:29:47 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.080f3301287d5340f8a1d48d48a2b331.js"
+  "/precache-manifest.780ff7e0edd158113304383a51428092.js"
 );
 
 self.addEventListener('message', (event) => {
