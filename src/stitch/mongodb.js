16a035ca34185f1d2e3b0bb9d22a5f38e1383c2d
Kevin Wu <kwu78@emory.edu>
2021-08-12 01:11:53 -0400

    updated data for 08/10

--- src/stitch/mongodb.js
+++ src/stitch/mongodb.js
@@ -10,6 +10,7 @@ const mongoClient = app.getServiceClient(
 const DB_NAME = 'healthequity';
 const CHED_series = mongoClient.db(DB_NAME).collection("CHED_series");
 const CHED_static = mongoClient.db(DB_NAME).collection("CHED_static");
+const GADPH_series=mongoClient.db(DB_NAME).collection("GADPH_series");
 
 
-export { CHED_static, CHED_series};
+export { CHED_static, GADPH_series,CHED_series};
