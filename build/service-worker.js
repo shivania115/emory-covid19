14f30d25de48dfa32ae31d1774c7d7bff2882963
Yuxuan Shi <1051268839@qq.com>
2023-09-11 22:13:26 -0400

    Update Decision Aid Elicit
    Bug fixes - step 2 modal window won't automatically pop up because it waits for cookies from step 1, which is currently behind step 5. Fixed by automatically insert dummy cookie when renders.

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.906ada350a8bad0c304c37fe22897d0a.js"
+  "/precache-manifest.b6de10b442fd625811d20c70ada6dc83.js"
 );
 
 self.addEventListener('message', (event) => {
