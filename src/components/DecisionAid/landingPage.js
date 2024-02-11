b16281039ed04f3c3c0bcd96d3fb570de3bc87fa
Yuxuan Shi <1051268839@qq.com>
2024-02-10 19:24:25 -0500

    Update landingPage.js
    LandingPage.js is now made responsive to mobile/tablet devices (screen width smaller than 1024px)

--- src/components/DecisionAid/landingPage.js
+++ src/components/DecisionAid/landingPage.js
@@ -63,150 +63,314 @@ i18n
     },
   });
 function LandingPage(props) {
+  // handle screen size change to conditionally render page
+  const [isMobileView, setIsMobileView] = useState(window.innerWidth < 1024);
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobileView(window.innerWidth < 1024);
+    };
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+  // end of screen size monitor
   const { t } = useTranslation();
   const location = useLocation();
   const currentPath = location.pathname;
   console.log(currentPath);
   console.log(props.elicit);
-  return (
-    <div
-      style={{
-        height: "100%",
-        width: "100%",
-        backgroundImage: "url(/DA_pic/vaccine2.png)",
-      }}
-    >
-      <center>
-        <ToggleButtonGroup
-          color="primary"
-          value={i18n.language}
-          size="small"
-          exclusive
-          onChange={(e, value) => {
-            i18n.changeLanguage(value);
-            console.log(value);
-            console.log(i18n.language);
-            // console.log(value);
-            // if(language!=value){
-            //   i18n.changeLanguage(value);
-            //   setLanguage(value);}
-          }}
-          aria-label="Platform"
-          style={{ paddingBottom: 0, paddingTop: 20 }}
-        >
-          <ToggleButton style={{ width: 200, fontSize: "1.25rem" }} value="en">
-            English
-          </ToggleButton>
-          <ToggleButton
+  // Mobile View: < 1024 px------------------------------------------------------------
+  if (isMobileView) {
+    return (
+      <div
+        style={{
+          height: "100vh",
+          width: "100%",
+          backgroundImage: "url(/DA_pic/vaccine2.png)",
+        }}
+      >
+        <center>
+          <ToggleButtonGroup
+            color="primary"
+            value={i18n.language}
+            size="small"
+            exclusive
+            onChange={(e, value) => {
+              i18n.changeLanguage(value);
+              console.log(value);
+              console.log(i18n.language);
+            }}
+            aria-label="Platform"
+            style={{ paddingBottom: 0, paddingTop: 20 }}
+          >
+            {/* <ToggleButton style={{ width: 200, fontSize: "1.25rem" }} value="en"> */}
+            <ToggleButton
+              style={{ width: "50%", fontSize: "1.25rem" }}
+              value="en"
+            >
+              English
+            </ToggleButton>
+            {/* <ToggleButton
             style={{ width: 200, fontSize: "1.25rem" }}
             value="span"
+          > */}
+            <ToggleButton
+              style={{ width: "50%", fontSize: "1.25rem" }}
+              value="span"
+            >
+              Español
+            </ToggleButton>
+          </ToggleButtonGroup>
+          {/* Headers */}
+          <Header
+            as="h2"
+            style={{ paddingTop: 10, fontWeight: 1000, fontSize: "2rem" }}
           >
-            Español
-          </ToggleButton>
-        </ToggleButtonGroup>
-        <Header
-          as="h2"
-          style={{ paddingTop: 25, fontWeight: 1000, fontSize: "3rem" }}
-        >
-          <Header.Content>{t("landingpage")}</Header.Content>
+            <Header.Content>{t("landingpage")}</Header.Content>
 
-          <HeaderSubHeader
-            style={{
-              paddingTop: "1.5rem",
-              paddingLeft: "2rem",
-              paddingBottom: "1rem",
-              lineHeight: "20pt",
-              fontWeight: 500,
-              fontSize: "2rem",
-              color: "black",
-            }}
-          >
-            <FontAwesomeIcon
-              icon={faCircleCheck}
+            <HeaderSubHeader
               style={{
-                fontSize: "3rem",
-                marginRight: "20pt",
-                color: "#024174",
+                paddingTop: "1.5rem",
+                // paddingLeft: "2rem",
+                paddingBottom: "0.5rem",
+                lineHeight: "20pt",
+                fontWeight: 500,
+                fontSize: "1.5rem",
+                color: "black",
               }}
-            />
-            {t("lp1")}
-          </HeaderSubHeader>
-        </Header>
-        <List as="ul">
-          <List.Item
-            style={{
-              paddingTop: "1rem",
-              paddingBottom: "0.5rem",
-              lineHeight: "20pt",
-              fontWeight: 400,
-              fontSize: "1.5rem",
-            }}
-            as="li"
-          >
-            {t("lp2")}
-          </List.Item>
-          <List.Item
-            style={{
-              paddingTop: "0.5rem",
-              paddingBottom: "0.5rem",
-              lineHeight: "20pt",
-              fontWeight: 400,
-              fontSize: "1.5rem",
-            }}
-            as="li"
-          >
-            {t("lp3")}
-          </List.Item>
-          <List.Item
-            style={{
-              paddingTop: "0.5rem",
-              paddingBottom: "0.5rem",
-              lineHeight: "20pt",
-              fontWeight: 400,
-              fontSize: "1.5rem",
-            }}
-            as="li"
-          >
-            {t("lp4")}
-          </List.Item>
-          <List.Item
-            style={{
-              paddingTop: "0.5rem",
-              paddingBottom: "0.5rem",
-              lineHeight: "20pt",
-              fontWeight: 400,
-              fontSize: "1.5rem",
-            }}
-            as="li"
-          >
-            {t("lp5")}
-          </List.Item>
-        </List>
-        {/* <Image size='large' src='/DA_pic/vaccine.jpg' >
+            >
+              <FontAwesomeIcon
+                icon={faCircleCheck}
+                style={{
+                  fontSize: "2rem",
+                  marginRight: "10pt",
+                  color: "#024174",
+                }}
+              />
+              {t("lp1")}
+            </HeaderSubHeader>
+          </Header>
+          <List as="ul">
+            <List.Item
+              style={{
+                paddingTop: "1rem",
+                paddingBottom: "0.5rem",
+                lineHeight: "20pt",
+                fontWeight: 400,
+                fontSize: "1.2rem",
+              }}
+              as="li"
+            >
+              {t("lp2")}
+            </List.Item>
+            <List.Item
+              style={{
+                paddingTop: "0.5rem",
+                paddingBottom: "0.5rem",
+                lineHeight: "20pt",
+                fontWeight: 400,
+                fontSize: "1.2rem",
+              }}
+              as="li"
+            >
+              {t("lp3")}
+            </List.Item>
+            <List.Item
+              style={{
+                paddingTop: "0.5rem",
+                paddingBottom: "0.5rem",
+                lineHeight: "20pt",
+                fontWeight: 400,
+                fontSize: "1.2rem",
+              }}
+              as="li"
+            >
+              {t("lp4")}
+            </List.Item>
+            <List.Item
+              style={{
+                paddingTop: "0.5rem",
+                paddingBottom: "0.5rem",
+                lineHeight: "20pt",
+                fontWeight: 400,
+                fontSize: "1.2rem",
+              }}
+              as="li"
+            >
+              {t("lp5")}
+            </List.Item>
+          </List>
+          {/* <Image size='large' src='/DA_pic/vaccine.jpg' >
         </Image> */}
-        {props.elicit ? (
-          <Link to="/decision-aid_elicit/step2">
-            <button
-              style={{ marginTop: "3rem", marginBottom: "10%" }}
-              class="ui massive primary button"
+          {props.elicit ? (
+            <Link to="/decision-aid_elicit/step2">
+              <button
+                style={{ marginTop: "3rem", marginBottom: "10%" }}
+                class="ui massive primary button"
+              >
+                {t("start")}
+              </button>
+            </Link>
+          ) : (
+            // button links to step1
+            <Link to="/decision-aid/step2">
+              {/* <Link to="/decision-aid/step3"> */}
+              <button
+                style={{ marginTop: "2rem", marginBottom: "10%" }}
+                class="ui massive primary button"
+              >
+                {t("start")}
+              </button>
+            </Link>
+          )}
+        </center>
+      </div>
+    );
+  } else {
+    // Desktop View: > 1024 px------------------------------------------------------------
+    return (
+      <div
+        style={{
+          height: "100%",
+          width: "100%",
+          backgroundImage: "url(/DA_pic/vaccine2.png)",
+        }}
+      >
+        <center>
+          <ToggleButtonGroup
+            color="primary"
+            value={i18n.language}
+            size="small"
+            exclusive
+            onChange={(e, value) => {
+              i18n.changeLanguage(value);
+              console.log(value);
+              console.log(i18n.language);
+            }}
+            aria-label="Platform"
+            style={{ paddingBottom: 0, paddingTop: 20 }}
+          >
+            {/* <ToggleButton style={{ width: 200, fontSize: "1.25rem" }} value="en"> */}
+            <ToggleButton
+              style={{ width: "50%", fontSize: "1.25rem" }}
+              value="en"
             >
-              {t("start")}
-            </button>
-          </Link>
-        ) : (
-          // button links to step1
-          <Link to="/decision-aid/step2">
-            {/* <Link to="/decision-aid/step3"> */}
-            <button
-              style={{ marginTop: "3rem", marginBottom: "10%" }}
-              class="ui massive primary button"
+              English
+            </ToggleButton>
+            {/* <ToggleButton
+            style={{ width: 200, fontSize: "1.25rem" }}
+            value="span"
+          > */}
+            <ToggleButton
+              style={{ width: "50%", fontSize: "1.25rem" }}
+              value="span"
             >
-              {t("start")}
-            </button>
-          </Link>
-        )}
-      </center>
-    </div>
-  );
+              Español
+            </ToggleButton>
+          </ToggleButtonGroup>
+          <Header
+            as="h2"
+            style={{ paddingTop: 25, fontWeight: 1000, fontSize: "3rem" }}
+          >
+            <Header.Content>{t("landingpage")}</Header.Content>
+
+            <HeaderSubHeader
+              style={{
+                paddingTop: "1.5rem",
+                paddingLeft: "2rem",
+                paddingBottom: "1rem",
+                lineHeight: "20pt",
+                fontWeight: 500,
+                fontSize: "2rem",
+                color: "black",
+              }}
+            >
+              <FontAwesomeIcon
+                icon={faCircleCheck}
+                style={{
+                  fontSize: "3rem",
+                  marginRight: "20pt",
+                  color: "#024174",
+                }}
+              />
+              {t("lp1")}
+            </HeaderSubHeader>
+          </Header>
+          <List as="ul">
+            <List.Item
+              style={{
+                paddingTop: "1rem",
+                paddingBottom: "0.5rem",
+                lineHeight: "20pt",
+                fontWeight: 400,
+                fontSize: "1.5rem",
+              }}
+              as="li"
+            >
+              {t("lp2")}
+            </List.Item>
+            <List.Item
+              style={{
+                paddingTop: "0.5rem",
+                paddingBottom: "0.5rem",
+                lineHeight: "20pt",
+                fontWeight: 400,
+                fontSize: "1.5rem",
+              }}
+              as="li"
+            >
+              {t("lp3")}
+            </List.Item>
+            <List.Item
+              style={{
+                paddingTop: "0.5rem",
+                paddingBottom: "0.5rem",
+                lineHeight: "20pt",
+                fontWeight: 400,
+                fontSize: "1.5rem",
+              }}
+              as="li"
+            >
+              {t("lp4")}
+            </List.Item>
+            <List.Item
+              style={{
+                paddingTop: "0.5rem",
+                paddingBottom: "0.5rem",
+                lineHeight: "20pt",
+                fontWeight: 400,
+                fontSize: "1.5rem",
+              }}
+              as="li"
+            >
+              {t("lp5")}
+            </List.Item>
+          </List>
+          {/* <Image size='large' src='/DA_pic/vaccine.jpg' >
+        </Image> */}
+          {props.elicit ? (
+            <Link to="/decision-aid_elicit/step2">
+              <button
+                style={{ marginTop: "3rem", marginBottom: "10%" }}
+                class="ui massive primary button"
+              >
+                {t("start")}
+              </button>
+            </Link>
+          ) : (
+            // button links to step1
+            <Link to="/decision-aid/step2">
+              {/* <Link to="/decision-aid/step3"> */}
+              <button
+                style={{ marginTop: "3rem", marginBottom: "10%" }}
+                class="ui massive primary button"
+              >
+                {t("start")}
+              </button>
+            </Link>
+          )}
+        </center>
+      </div>
+    );
+  }
 }
 export default LandingPage;
